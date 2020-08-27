34e40eef2fdad146bd85c73bb7a72fe51a590837
Ivan Aponte <ivan.aponte@gmail.com>
2020-08-27 17:57:49 -0400

    update files

--- app.js
+++
@@ -1,8 +0,0 @@
-const ipc = require("electron").ipcRenderer
-console.log("begin")
-
-ipc.on("changeWeb", (event, message) => {
-  console.log("received", message)
-  const frame = document.getElementById("framex")
-  frame.src = message
-})
